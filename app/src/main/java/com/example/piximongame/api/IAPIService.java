2ec07760fad3dd09a35cf7bea19c0f798f5be8fd
Sheila <sheilajnieto@gmail.com>
2024-02-19 11:48:59 +0100

    -Añadiento comentarios en IAPIService, para entender para qué cada endpoint.

--- app/src/main/java/com/example/piximongame/api/IAPIService.java
+++ app/src/main/java/com/example/piximongame/api/IAPIService.java
@@ -9,6 +9,7 @@ import retrofit2.http.POST;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
+import com.example.piximongame.entidades.Carta;
 import com.example.piximongame.entidades.Jugador;
 import com.example.piximongame.entidades.Partida;
 import com.example.piximongame.entidades.Usuario;
@@ -19,9 +20,7 @@ import java.util.List;
 //Esta interfaz define los métodos que se utilizarán para hacer las peticiones a la API
 public interface IAPIService {
 
-     @POST("/digimons/recuperar")
-     Call<ResponseStatus> recuperarDigimons();
-
+     //Al logearnos con éxito, se cargarán digimons en la bbdd de la api si no existen ya en la bbdd
      @POST("/digimons/comprobarDigimonsEnBBDD")
      Call<ResponseStatus> insertarDigimonsEnBBDD();
 
@@ -29,6 +28,7 @@ public interface IAPIService {
     @POST("/usuarios/registrar")
     Call<ResponseStatus> registrarUsuario(@Body Usuario usuario);
 
+    //Al ingresar con un usuario en pantalla login, comprobar si existe y si es correcto
     @POST("/usuarios/comprobarLogin")
     Call<ResponseStatus> comprobarLogin(@Body Usuario usuario);
 
@@ -36,15 +36,20 @@ public interface IAPIService {
     @POST("/jugadores/addJugadores")
     Call<ResponseStatus> guardarJugadores(@Body Jugador jugadores);
 
-    @GET("/jugadores/getJugadores")
-    Call<List<Jugador>> obtenerJugadoresEnPartida();
 
+    //Para obtener todos los jugadores de una partida concreta (nos devolverá los 5 jugadores)
     @GET("/jugadores/obtenerJugadoresPorPartidaId(int idPartida)")
     Call<List<Jugador>> obtenerJugadoresPorPartidaId(int idPartida);
 
+    //Para obtener todos los jugadores bots de una partida concreta
     @GET("jugadores/obtenerJugadoresAleatoriosEnPartida")
     Call<List<Jugador>> obtenerJugadoresAleatoriosEnPartida(@Query("idPartida") int idPartida);
 
+    //Para obtener las cartas de un jugador concreto
+    @GET("/cartas/obtenerCartasJugador")
+    Call<List<Carta>> obtenerCartasJugador(@Query("idJugador") int idJugador);
+
+    //Para obtener la partida actual donde pertenecen los 5 jugadores del juego actual
     @GET("/partida/obtenerPartidaActual")
       Call<Partida> obtenerPartidaActual(@Query("nombre") String nombreUsuarioJugador);
 
